34ef3bda53bac44f4def5a1416298fb1833c2fee
myz <361845260@qq.com>
2022-08-17 19:41:57 +0800

    third111 commit

--- src/main/java/com/xxl/job/XxljobApplication.java
+++ src/main/java/com/xxl/job/XxljobApplication.java
@@ -10,6 +10,7 @@ public class XxljobApplication {
         System.out.println("-------->1111<-------");
         System.out.println("2---2");
         System.out.println("33----33");
+        System.out.println("00000");
         SpringApplication.run(XxljobApplication.class, args);
     }
 
